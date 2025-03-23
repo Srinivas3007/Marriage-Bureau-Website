1266626a9daeb2990063ff240c72d3917fe2c738
Srinivas1529 <srinivasmsdp@gmail.com>
2025-03-23 12:44:18 +0530

    axios code is working fine

--- front_end/react-app-for-matrimony-website/src/App.jsx
+++ front_end/react-app-for-matrimony-website/src/App.jsx
@@ -2,32 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import apiConfig from './api/apiConfig'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Person, setPerson] = useState();
+
+  const getPerson = async () =>{
+    try{
+      const response = await apiConfig.get('/mongo/getAll');
+      console.log(response.data);
+      setPerson(response.data);
+    }catch(err){
+      console.log(err);
+    }
+    
+  }
+
+  useEffect(()=>{
+    getPerson();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
