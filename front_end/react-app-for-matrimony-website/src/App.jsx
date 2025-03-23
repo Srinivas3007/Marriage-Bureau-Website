91af55c36d05eeb0d1db6efa8c6c0f9f1b7eb4f3
Srinivas1529 <srinivasmsdp@gmail.com>
2025-03-23 17:29:37 +0530

    added arrays and objects for props

--- front_end/react-app-for-matrimony-website/src/App.jsx
+++ front_end/react-app-for-matrimony-website/src/App.jsx
@@ -2,11 +2,13 @@ import Learn from './Properties/Learn'
 
 
 function App() {
+  const features =[10, 20, 30]
+  const featuresObj ={a:1, b:2, c:"amazon", d:"facebook"}
   return (
     <>
       <div className="propDisplay">
-        <Learn title="Properties In React" desc="This is a demo on using properties in React" price="30K"></Learn>
-        <Learn title="React Props demo" desc="React demo app for illustrating Props concept" price={25000}></Learn>
+        <Learn title="Properties In React" desc="This is a demo on using properties in React" price="30K" features={features} featuresObj={featuresObj}></Learn>
+        <Learn title="React Props demo" desc="React demo app for illustrating Props concept" price={25000} ></Learn>
       </div>
     </>
   );
--- front_end/react-app-for-matrimony-website/src/Properties/Learn.jsx
+++ front_end/react-app-for-matrimony-website/src/Properties/Learn.jsx
@@ -1,8 +1,10 @@
 import './Learn.css'
-export default function Learn({title, desc, price}){
+export default function Learn({title, desc, price, features=[1,2,3], featuresObj={a:10,b:"amazon"}}){
+    const amma = features.map((feature) => <li>{feature}</li>);
     return (<div className="learnDiv">
         <h4 className="head1">{title}</h4>
         <h6 className="desc">{desc}</h6>
         <h6 className="desc">{price}</h6>
+        <p>{amma}</p>
     </div>);
 }
